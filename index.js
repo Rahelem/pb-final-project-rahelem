9becfec592354a1c0e25376bc52c1c4282abedd6
Raphael <raphael.lesmann@dci-student.org>
2025-06-30 05:09:26 +0200

    final commit

--- index.js
+++ index.js
@@ -40,12 +40,13 @@ const characterRoster = {
         flashKick: { damage: 15 },
         special: { damage: 22 }
     }
-  }
+  },
 };
 
-function createFighter(template) {
+function createFighter(templateName) {
+  const template = characterRoster[templateName];
     return {
-        name: template,
+        name: templateName,
         hp: template.hp,
         moves: template.moves,
         parryInit: false,
@@ -53,7 +54,7 @@ function createFighter(template) {
     };
 }
 
-function selectCharacter(playerName) {
+function chooseCharacter(playerName) {
     const names = Object.keys(characterRoster);
     console.log(`${playerName}, choose your character: ${names.join("")}`);
     const choice = readlineSync.question("> ");
@@ -64,8 +65,28 @@ function selectCharacter(playerName) {
     return createFighter(choice);
 }
 
+function parrySuccess() {
+  return Math.random() < 0.5;
+}
+
+function counterSuccess() {
+  return Math.random() < .25;
+}
+
 function attack(attacker, defender, moveKey) {
   console.log(`/n${attacker.naem} tries ${moveKey}!`);
+
+  if (moveKey === "parry") {
+    attacker.parryInit = true;
+    console.log(`${attacker.name} is ready to parry.`); 
+    return;
+  }
+
+  if (moveKey === "counter") {
+    attacker.counterInit = true; 
+    console.log(`${attacker.name} is ready to counter.`);
+    return
+  }
 
   const move = attacker.moves[moveKey];
   if (!move) {
@@ -74,4 +95,54 @@ function attack(attacker, defender, moveKey) {
   }
 
   let dmg = move.damage;
+
+if (defender.parryInit && parrySuccess()) {
+  console.log(`${defender.name} parried and countre-hits for ${dmg} damage!`);
+  attacker.hp -= dmg;
+  defender.parrySuccess = false;
+  return;
 }
+
+if (defender.counterInit && counterSuccess()) {
+    console.log(`${defender.name} counters! ${attacker.name} takes ${dmg*2} damage!`);
+    attacker.hp -= dmg * 2;
+    defender.counterSucces = false;
+    return;
+  }
+
+
+console.log(`${attacker.name} hits ${defender.name} for ${dmg} damage.`);
+defender.hp -= dmg;
+
+defender.parryInit = false;
+defender.counterInit = false;
+}
+
+
+
+
+function startGame() {
+  console.log("STREET TERMINAL FIGHTER");
+  const p1 = chooseCharacter("Player 1");
+  const p2 = chooseCharacter("Player 2");
+  let round = 1;
+
+  while (p1.hp > 0 && p2.hp > 0) {
+    console.log(`\n--- Round ${round} ---`);
+    console.log(`${p1.name}: ${p1.hp} HP   |   ${p2.name}: ${p2.hp} HP`);
+
+    const m1 = readlineSync.question(`${p1.name}, pick a move (${Object.keys(p1.moves).concat(["parry","counter"]).join(", ")}): `);
+    attack(p1, p2, m1);
+    if (p2.hp <= 0) break;
+
+    const m2 = readlineSync.question(`${p2.name}, pick a move (${Object.keys(p2.moves).concat(["parry","counter"]).join(", ")}): `);
+    attack(p2, p1, m2);
+
+    round++;
+  }
+
+  const winner = p1.hp > 0 ? p1.name : p2.name;
+  console.log(`\n ${winner} wins!`);
+}
+
+startGame();
